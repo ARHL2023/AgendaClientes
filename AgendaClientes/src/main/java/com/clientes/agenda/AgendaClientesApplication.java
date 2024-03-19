d457220116401a34ad4a9b87660552f38de1f6b1
ARHL2023 <TuCorreoElectronico>
2024-03-18 21:09:22 -0500

    Primer commit: Agregar archivos iniciales

---
+++ AgendaClientes/src/main/java/com/clientes/agenda/AgendaClientesApplication.java
@@ -0,0 +1,13 @@
+package com.clientes.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaClientesApplication.class, args);
+	}
+
+}
